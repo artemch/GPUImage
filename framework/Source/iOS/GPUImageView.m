6fe9b72dd9fa9791d42974d8e6da9869fe6c5960
Artem Chabannyi <artem.chabanniy@gmail.com>
2017-05-11 12:27:24 +0300

    Chnage glBlendFunc param.

--- framework/Source/iOS/GPUImageView.m
+++ framework/Source/iOS/GPUImageView.m
@@ -378,7 +378,7 @@
         
         if (self.transparent) {
             glEnable(GL_BLEND);
-            glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         }
         glClearColor(backgroundColorRed, backgroundColorGreen, backgroundColorBlue, backgroundColorAlpha);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
